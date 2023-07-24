dd969d54b800d7d9c5eb1b7b9cb3fdb53d659159
Nivaniz <Nivaniz2019@gmail.com>
2023-07-23 19:09:06 -0600

    Prueba

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -11,21 +11,13 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import to include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('services/', views.services, name="services"),
-    path('store/', views.store, name="store"),
-    path('contact/', views.contact, name="contact"),
-    path('blog/', views.blog, name="blog"),
-    path('sample/', views.sample, name="sample"),
     path('admin/', admin.site.urls),
 ]
